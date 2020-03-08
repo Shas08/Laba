21ac2520f40a5a02783706cf97ea4adb34ac8524
Андрей Шапоренко <andrejsaporenko@MacBook-Air-Andrej.local>
2020-03-08 19:12:51 +0700

    in process...

--- ACSI/acsi.cpp
+++ ACSI/acsi.cpp
@@ -1,19 +1,61 @@
 #include<iostream>
 #include<fstream>
+#include<vector>
 using namespace std;
+
+struct knot
+{
+    vector<char> symb;
+    vector<int> numb;
+};
+
+int index_of_max(knot noeds)
+{
+    int max=noeds.numb[0], k=0;
+    for(int i=1;i<255;i++) 
+        if(max<noeds.numb[i]) k=i;
+    return k; 
+}
+
+int index_of_min(knot noeds)
+{
+    int k=max(noeds), min=noeds.numb[k];
+    for(int i=1;i<255;i++)  
+        if(min>noeds.numb[i] && noeds.numb[i]!=0) k=i;    
+    return k;
+}
+
+void without_0(knot &noeds)
+{
+    int n=noeds.numb.size();
+    for(int i=0; i<n; i++)
+        if(noeds.numb[i]==0)
+        {
+            noeds.numb.erase(i);
+            noeds.symb.erase(i);
+        }
+}
+
 int main()
 {
+    knot noeds;
     char symb;
-    int *b1= new int[255], j=0;
-    for(int i=0; i<255; i++) b1[i]=0;
+    int j=0, k=0, max_grade;
+    for(int i=0; i<255; i++) 
+    {
+        noeds.numb.push_back(0);
+        noeds.symb.push_back((int)i);
+    }
     ifstream txt("text.txt");
     while((symb=txt.get())!=EOF)
     {
-        b1[(int)symb]++;
+        noeds.numb[(int)symb]=noeds.numb[(int)symb]+1;
         j++;
     }
     txt.close();
-    Piramid(b1, 255);
-    for(int i=0; i<255; i++) cout<<b1[i]<<endl;
+
+    max_grade=j*8;
+    //cout<<max_grade<<endl;
+    //for(int i=0; i<255; i++) cout<<noeds.numb[i]<<"    "<<noeds.symb[i]<<endl;
     return 0;
 }
